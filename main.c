3644b567e30b98930b1eb02d1c252a3128f5b06f
Prakash Bhattarai <prakash1bhattarai25@gmail.com>
2019-09-17 13:40:58 +0545

    works as a wallpaper

--- main.c
+++ main.c
@@ -8,11 +8,15 @@ int main(int argc, char *argv[])
 {
 	// Initialize GTK+
 	gtk_init(&argc, &argv);
-
+	GdkRectangle workarea = {0};
+	gdk_monitor_get_workarea(gdk_display_get_primary_monitor(gdk_display_get_default()),
+    &workarea);
+	// printf("%d %d\n",workarea.width, workarea.height);
 	// Create an 800x600 window that will contain the browser instance
 	GtkWidget *main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_default_size(GTK_WINDOW(main_window), 800, 600);
-
+	// GtkWidget *main_window = gtk_window_new(GTK_WINDOW_POPUP);
+	gtk_window_set_default_size(GTK_WINDOW(main_window), 300, 300);
+	
 	// Create a browser instance
 	WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
 
@@ -23,7 +27,8 @@ int main(int argc, char *argv[])
 	gtk_widget_set_visual(main_window, visual);
 	gtk_widget_set_visual(GTK_WIDGET(webView), visual);
 	webkit_web_view_set_background_color(webView, &color);
-
+	gtk_widget_set_sensitive(GTK_WIDGET(webView),FALSE);
+	// gtk_widget_set_focus_on_click(GTK_WIDGET(webView),FALSE);
 	// gtk_widget_override_background_color(main_window, GTK_STATE_FLAG_NORMAL, &color);
 	// gtk_widget_override_background_color(GTK_WIDGET(webView), GTK_STATE_FLAG_NORMAL, &color);
 	// Put the browser area into the main window
@@ -35,7 +40,7 @@ int main(int argc, char *argv[])
 	g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCb), main_window);
 
 	// Load a web page into the browser instance
-	webkit_web_view_load_uri(webView, "file:///home/prakash/selfuniv/programming/gtkprograms/index.html");
+	webkit_web_view_load_uri(webView, "file:///home/prakash/projects/widget/desktop-widget/index.html");
 
 	// Make sure that when the browser area becomes visible, it will get mouse
 	// and keyboard events
@@ -45,14 +50,23 @@ int main(int argc, char *argv[])
 	// Make sure the main window and all its contents are visible
 
 	gtk_widget_show_all(main_window);
-	gtk_window_set_keep_below(GTK_WINDOW(main_window), TRUE);
-	gtk_window_set_type_hint(GTK_WINDOW(main_window), GDK_WINDOW_TYPE_HINT_DOCK);
+	gtk_window_set_type_hint(GTK_WINDOW(main_window), GDK_WINDOW_TYPE_HINT_DESKTOP);
+
+	// gtk_window_set_type_hint(GTK_WINDOW(main_window), GDK_WINDOW_TYPE_HINT_DOCK);
+	// gtk_window_set_type_hint(GTK_WINDOW(main_window), GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
+	
 
 	//prevent the window from appearing in taskbar
 	gtk_window_set_skip_taskbar_hint(GTK_WINDOW(main_window), TRUE);
 	//prevent from appering in tab switch (not working)
 	gtk_window_set_skip_pager_hint(GTK_WINDOW(main_window), FALSE);
+	gtk_window_set_accept_focus(GTK_WINDOW(main_window), FALSE);
+	// gtk_window_set_position(GTK_WINDOW(main_window),(GTK_WIN_POS_CENTER));
 	// Run the main GTK+ event loop
+	gtk_window_set_keep_below(GTK_WINDOW(main_window), TRUE);
+	gtk_window_set_gravity(GTK_WINDOW(main_window),GDK_GRAVITY_EAST);
+	gtk_window_move(GTK_WINDOW(main_window), workarea.width/2,workarea.height/2);
+	// gtk_widget_unrealize(GTK_WIDGET(main_window));
 	gtk_main();
 
 	return 0;
